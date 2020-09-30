8bc58f64f642d6460b25e2c0e833d51773826de9
sivohin_a <sivohin_a@protei.ru>
2020-09-30 13:46:10 +0300

    init complete

---
+++ ui/src/js/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const rootEl = document.getElementById('root');
+ReactDOM.render(
+    <div>Hello</div>,
+    rootEl
+);
---
+++ ui/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        path: path.join(__dirname, "target/dist/static/js"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+};
